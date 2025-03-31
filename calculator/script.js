61322c1659c26ca9af0a3a09f7a3d98ee8104492
noahweasley <iebnmelu@gmail.com>
2025-03-31 13:01:31 +0100

    Remove additional listener

--- calculator/script.js
+++ calculator/script.js
@@ -12,8 +12,6 @@ for (const button of buttons) {
   //   const target = event.target;
   //   //
   // });
-  button.addEventListener("touchstart", (event) => {});
-  button.addEventListener("click", (event) => {});
 
   // const events = ["click", "touchstart"];
   // for (let event of events) {
@@ -21,7 +19,7 @@ for (const button of buttons) {
   // }
 
   //
-  ["click", "touchstart"].forEach((event) =>
+  ["click"].forEach((event) =>
     button.addEventListener(event, handleButtonClick)
   );
 }
